8dcc02f0745e988aaee2ee1b49792e7eba5811b2
Lauren Dove <elledoveinc@gmail.com>
2019-09-25 01:52:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
-class Dog
-  def name #(dog_name)
-    @the_name #= dog_name
-  end
-  def name=(dog_name) 
-   @the_name =dog_name
+class Dog 
+  def name=(name)
+    @name = name
   end
 end
-fido = Dog.new
-fido.name = "Fido"
-fido.name
